4e62ae41ab2b9eb40524ab615b78c70201ff903a
franciscoseita <105219328+franciscoseita@users.noreply.github.com>
2024-12-05 12:06:19 -0600

    Update schema.sql

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,8 +1,8 @@
--- Crear la tabla si no existe
+-- Crear la base de datos si no existe
 CREATE TABLE IF NOT EXISTS usuarios (
     id BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) DEFAULT NULL,
     email VARCHAR(30) DEFAULT NULL,
     password VARCHAR(16) DEFAULT NULL,
-    isLogged TINYINT(1) DEFAULT NULL
+    isLogged TINYINT(1) DEFAULT NULL
 );
